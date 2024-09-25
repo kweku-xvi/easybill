c7b91aef715b320f68643ef57612be638fb22576
kweku-xvi <nksarps@gmail.com>
2024-09-25 18:06:35 +0000

    created db model for users

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,61 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is Required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        extra_fields.setdefault("is_verified",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+        if extra_fields.get("is_verified") is not True:
+            raise ValueError(_("SuperUser must have is_verified=True"))
+
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    id = models.CharField(max_length=12, unique=True, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(unique=True, max_length=30)
+    business_name = models.CharField(max_length=100)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "business_name", "phone_number"]
+
+
+    def __str__(self):
+        return self.username
+
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[24:37]
+        super().save(*args, **kwargs)
+
+
+    class Meta:
+        ordering = ("-created_at",)
