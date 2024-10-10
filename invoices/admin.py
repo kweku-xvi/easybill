151a42df10c77c7ef7a220ed6b2981040b1558ac
kweku-xvi <nksarps@gmail.com>
2024-10-10 17:29:40 +0000

    added invoices to items in invoices to admin panel

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,2 +1,15 @@
+from .models import Invoice, InvoiceItem
 from django.contrib import admin
 
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'sub_total', 'sender')
+    readonly_fields = ('created_at',)
+
+
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'quantity', 'unit_price', 'invoice')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(InvoiceItem, InvoiceItemAdmin)
